cb8f6f34f06de8737a8a159dcd6b61225c12fd6d
REESE <u0690525@umail.utah.edu>
2021-09-05 01:01:15 -0600

    Adding ungraded tester projects to this repo.  For PS1 of CS2700

---
+++ TestFormEval/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using FormulaEvaluator;
+
+namespace TestFormEval
+{
+    class TestFormulaEvaluator
+    {
+        static void Main(string[] args)
+        {
+            string a = "3";
+            string s = "(2+35)*A7"; //["(", "2", "+", "35", ")", "*", "A7"] from given regex
+            string r = "(2+35)* A7";
+            
+
+            string t = "E96F";
+
+            string[] testvariable = Regex.Split(a, "[A-Z]+");
+
+            int x =  Evaluator.Evaluate(a, TestLookup);
+
+            foreach (string test1 in testvariable)
+            {
+                Console.Write(test1 + "\n");
+               //int? number = IsNum(test1);
+                Console.WriteLine("Printing ints" + x);
+                Console.WriteLine(x + 1);
+
+
+            }
+           Console.WriteLine(x);
+           
+            
+            
+        }
+        
+     public static int TestLookup(string s)
+        {
+            return 2;
+        }
+
+        static int? IsNum(string s)
+        {            
+            int x;
+          bool yesorno = int.TryParse(s, out x);
+            if (yesorno)
+                return x;
+            else
+                return null;             
+        }
+
+    }
+}
